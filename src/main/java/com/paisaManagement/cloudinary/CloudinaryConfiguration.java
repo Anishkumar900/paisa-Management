13608371c5ea70b831b37b178927a9d494cb69ee
Anishkumar900 <anishunique900@gmail.com>
2025-08-25 08:01:36 +0530

    bank update

--- src/main/java/com/paisaManagement/cloudinary/CloudinaryConfiguration.java
+++ src/main/java/com/paisaManagement/cloudinary/CloudinaryConfiguration.java
@@ -1,6 +1,7 @@
 package com.paisaManagement.cloudinary;
 
 import com.cloudinary.Cloudinary;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +11,21 @@ import java.util.Map;
 @Configuration
 public class CloudinaryConfiguration {
 
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary(){
         final Map<String,String> config=new HashMap<>();
-        config.put("cloud_name","dvtr8vpwc");
-        config.put("api_key","729253337763344");
-        config.put("api_secret","lYQvhR4HO9IBh4fXjtMWByq-CfQ");
+        config.put("cloud_name", cloudName);
+        config.put("api_key", apiKey);
+        config.put("api_secret", apiSecret);
         return new Cloudinary(config);
     }
 }
